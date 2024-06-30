c34ab6afba156fcaad11b80b5bd87791d6498ceb
datnguyentb <datnguyen.github@gmail.com>
2024-06-30 22:24:27 +0700

    update left-slidebar

---
+++ database/Acount/index.js
@@ -0,0 +1,28 @@
+const Acounts = [
+    {
+        username: 'dat.nt170499',
+        password: 'Dat170499',
+        name: "Dat Nguyen",
+        roman_copy: "",
+        detal: nomal_username,
+        avatar: "./",
+        rate: '100',
+        vip: true,
+        playlist: [
+            {
+                title: "Like song",
+                code: ls,
+                part_number: 1,
+                songs:['song1', 'song2', 'song3', 'song4']
+            },
+            {
+                title: "Like song",
+                code: ls,
+                part_number: 1,
+                songs:['song1', 'song2', 'song3', 'song4']
+            }
+        ]
+    }
+]
+
+export default Acounts
---
+++ src/components/User/User.js
@@ -0,0 +1,23 @@
+let current_user = {
+    name: "Dat Nguyen",
+    roman_copy: "",
+    avatar: "./",
+    rate: '100',
+    premium: false,
+    playlist: [
+        {
+            title: "Like song",
+            code: 'ls',
+            part_number: 1,
+            songs:['song1', 'song2', 'song3', 'song4']
+        },
+        {
+            title: "Like song",
+            code: 'ls',
+            part_number: 1,
+            songs:['song1', 'song2', 'song3', 'song4']
+        }
+    ]
+};
+
+export default current_user;
